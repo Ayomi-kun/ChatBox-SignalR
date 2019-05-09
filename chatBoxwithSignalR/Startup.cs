0e17d969bb3e4c7ee351a0e96b52a46d767be51e
Ayomiga <oluwayemiayo22@gmail.com>
2019-05-09 12:13:27 +0100

    startup class

---
+++ chatBoxwithSignalR/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chatBoxwithSignalR
+{
+    public partial class Startup
+    {
+        public void configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+            app.MapSignalR();
+        }
+
+    }
+
+    
+    public class ChatHub : Microsoft.AspNet.SignalR.Hub
+    {
+        //method to update broadcast message to client
+        public void Send (string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+
+}
